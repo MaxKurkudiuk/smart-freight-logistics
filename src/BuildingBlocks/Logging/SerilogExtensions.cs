ca98240880eac817b649ccce86e3ed3a1d459f81
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-08-30 17:40:05 +0300

    Add BuildingBlocks.Logging with Serilog and CorrelationId
    Added BuildingBlocks.Logging project targeting .NET 10.0 with Serilog.AspNetCore and Serilog.Sinks.Console for logging. Implemented CorrelationIdMiddleware to manage correlation IDs per HTTP request and integrate with Serilog log context. Provided SerilogExtensions for easy setup and DI registration. Updated solution to include the new project.

---
+++ src/BuildingBlocks/Logging/SerilogExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace BuildingBlocks.Logging;
+
+public static class SerilogExtensions
+{
+    public static WebApplicationBuilder AddSharedLogging(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        // Register the middleware in DI container automatically
+        builder.Services.AddTransient<CorrelationIdMiddleware>();
+
+        return builder;
+    }
+
+    public static WebApplication UseSharedLogging(this WebApplication app)
+    {
+        app.UseMiddleware<CorrelationIdMiddleware>();
+        return app;
+    }
+}
