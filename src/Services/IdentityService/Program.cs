266f3aa305ac48c8a2d8872d4ae6d921fef6b3fd
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-08-30 20:28:47 +0300

    Add EF Core, PostgreSQL, and User entity setup
    - Added UserSecretsId to project for secret management
    - Configured PostgreSQL connection and JWT in appsettings.json
    - Introduced IdentityDbContext with Users DbSet and constraints
    - Created User entity with core properties
    - Registered IdentityDbContext and Npgsql in Program.cs
    - Commented out controller mapping in Program.cs

--- src/Services/IdentityService/Program.cs
+++ src/Services/IdentityService/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocks.Logging;
+using IdentityService.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +11,24 @@ builder.AddSharedLogging();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var baseConnectionString = builder.Configuration.GetConnectionString("IdentityDb");
+var dbPassword = builder.Configuration["DatabaseSettings:Password"];
+
+var connectionBuilder = new NpgsqlConnectionStringBuilder(baseConnectionString)
+{
+    Password = dbPassword
+};
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString(connectionBuilder.ConnectionString)));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseSharedLogging();
 
 app.UseRouting();
-app.MapControllers();
+//app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
